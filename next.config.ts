efc2f34735545bdf3e2097b69b8ead1acc439496
Joonsu Yoon <bananamilk452@gmail.com>
2025-10-21 00:19:23 +0900

    chore(fedify): 요청마다 뜨는 rdf-canonize-native 오류 수정
    https://github.com/digitalbazaar/jsonld.js/issues/567

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "rdf-canonize-native": false,
+    };
+
+    return config;
+  },
 };
 
 export default nextConfig;
