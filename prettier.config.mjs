2f7a8c5bf0c791f170e798ebdc9487b0e0894524
Joonsu Yoon <bananamilk452@gmail.com>
2025-10-20 22:44:45 +0900

    Initial Commit

---
+++ prettier.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import("prettier").Config} */
+export default {
+  plugins: [
+    "@ianvs/prettier-plugin-sort-imports",
+    "prettier-plugin-tailwindcss",
+  ],
+  importOrder: [
+    "<THIRD_PARTY_MODULES>",
+    "",
+    "^~/(.*)$",
+    "",
+    "^[./]",
+    "",
+    "<TYPES>",
+  ],
+  tailwindFunctions: ["cn"],
+};
