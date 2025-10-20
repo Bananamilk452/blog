03993d9825afcc4d1023ac88de04f4ee3da675f4
Joonsu Yoon <bananamilk452@gmail.com>
2025-10-20 23:07:49 +0900

    feat(fedify): 최소한의 fedify 셋업 (도커 세팅 위해서 기본적인 것만)

---
+++ src/app/federation.ts
@@ -0,0 +1,27 @@
+import { createFederation, Person } from "@fedify/fedify";
+import { RedisKvStore, RedisMessageQueue } from "@fedify/redis";
+import { Redis } from "ioredis";
+
+const redisUrl = process.env.REDIS_URL;
+
+if (!redisUrl) {
+  throw new Error("REDIS_URL is not defined");
+}
+
+const federation = createFederation({
+  kv: new RedisKvStore(new Redis(redisUrl)),
+  queue: new RedisMessageQueue(() => new Redis(redisUrl)),
+});
+
+federation.setActorDispatcher(
+  "/users/{identifier}",
+  async (ctx, identifier) => {
+    return new Person({
+      id: ctx.getActorUri(identifier),
+      preferredUsername: identifier,
+      name: identifier,
+    });
+  },
+);
+
+export { federation };
