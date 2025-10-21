ea6c8ce320be5d720290f03932971d4e2af389f2
Joonsu Yoon <bananamilk452@gmail.com>
2025-10-21 13:12:55 +0900

    feat(better-auth): emaill과 password를 사용한 가입 및 인증 활성화

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -9,5 +9,8 @@ export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
+  emailAndPassword: {
+    enabled: true,
+  },
   plugins: [username()],
 });
