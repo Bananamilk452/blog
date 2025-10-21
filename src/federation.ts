891d7ee3a88f21bfd3364998e44c6ee06a8c6a7c
Joonsu Yoon <bananamilk452@gmail.com>
2025-10-21 13:14:05 +0900

    feat(fedify): 사용자 lookup 시 데이터베이스 내용과 연동되게 + inbox 핸들러 추가

--- src/federation.ts
+++ src/federation.ts
@@ -1,6 +1,8 @@
-import { createFederation, Person } from "@fedify/fedify";
+import { createFederation, Endpoints, Person } from "@fedify/fedify";
 import { RedisKvStore, RedisMessageQueue } from "@fedify/redis";
 import { Redis } from "ioredis";
+
+import { prisma } from "./lib/prisma";
 
 const redisUrl = process.env.REDIS_URL;
 
@@ -16,12 +18,27 @@ const federation = createFederation({
 federation.setActorDispatcher(
   "/users/{identifier}",
   async (ctx, identifier) => {
+    const user = await prisma.user.findFirst({
+      where: { username: identifier },
+    });
+
+    if (!user) {
+      return null;
+    }
+
     return new Person({
       id: ctx.getActorUri(identifier),
       preferredUsername: identifier,
-      name: identifier,
+      name: user.name,
+      inbox: ctx.getInboxUri(identifier),
+      endpoints: new Endpoints({
+        sharedInbox: ctx.getInboxUri(),
+      }),
+      url: ctx.getActorUri(identifier),
     });
   },
 );
+
+federation.setInboxListeners("/users/{identifier}/inbox", "/inbox");
 
 export { federation };
