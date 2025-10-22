40cf8092308081289609305c3441038abac62371
Joonsu Yoon <bananamilk452@gmail.com>
2025-10-22 19:27:05 +0900

    feat(frontend): 글 작성용 테스트 UI 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,30 @@
+"use client";
+
+import { useState } from "react";
+
+import { Button } from "~/components/ui/button";
+import { Textarea } from "~/components/ui/textarea";
+import { createPost } from "~/lib/actions/post";
+
 export default function Home() {
-  return <div>Hello, World!</div>;
+  const [content, setContent] = useState("");
+
+  function onSubmit() {
+    createPost(content).then(() => {
+      alert("Post created successfully!");
+      setContent("");
+    });
+  }
+
+  return (
+    <div className="flex w-lg flex-col gap-4">
+      <Textarea
+        className="h-64"
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+      />
+
+      <Button onClick={onSubmit}>Write</Button>
+    </div>
+  );
 }
