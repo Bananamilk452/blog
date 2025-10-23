3705e6e31d03555b04e4d17a6466e5c80b2e4299
Joonsu Yoon <bananamilk452@gmail.com>
2025-10-23 19:24:43 +0900

    feat(scripts): addUser 스크립트 admin 생성되게 수정

--- src/scripts/addUser.ts
+++ src/scripts/addUser.ts
@@ -12,7 +12,7 @@ program
   .requiredOption("-n, --name <name>", "name")
   .requiredOption("-u, --username <username>", "username")
   .requiredOption("-p, --password <password>", "password")
-  .option("--create-actor", "also create an associated actor");
+  .option("-a, --admin", "set user as admin");
 
 program.parse();
 const options = program.opts();
@@ -27,39 +27,14 @@ async function createUser() {
     },
   });
 
-  const user = await prisma.user.findFirst({ where: { email: options.email } });
-
-  if (!user) {
-    throw new Error("Cannot find user");
-  }
-
-  if (!process.env.PUBLIC_URL) {
-    throw new Error("PUBLIC_URL is not defined");
-  }
-
-  const url = new URL(process.env.PUBLIC_URL);
-
-  if (options.createActor) {
-    await prisma.actor.create({
-      data: {
-        userId: user.id,
-        uri: `${url.host}/users/${user.username}`,
-        handle: user.username,
-        name: user.name,
-        inboxUrl: `${url.host}/users/${user.username}/inbox`,
-        sharedInboxUrl: `${url.host}/inbox`,
-        url: `${url.origin}/users/${user.username}`,
-      },
+  if (options.admin) {
+    await prisma.user.update({
+      where: { email: options.email },
+      data: { role: "admin" },
     });
   }
 
-  if (options.createActor) {
-    console.log(
-      `✅ User created with actor: ${options.email} (${options.username})`,
-    );
-  } else {
-    console.log(`✅ User created: ${options.email} (${options.username})`);
-  }
+  console.log(`✅ User created: ${options.email} (${options.username})`);
 }
 
 createUser();
