4e9add5e04cbc7b0066d6eaf69f1fc44058ad77c
Joonsu Yoon <bananamilk452@gmail.com>
2025-10-24 11:52:49 +0900

    format: cva prettier tailwindFunction에 추가

--- src/components/ui/note.tsx
+++ src/components/ui/note.tsx
@@ -8,13 +8,13 @@ import {
 
 import type { VariantProps } from "class-variance-authority";
 
-const noteVariants = cva("flex flex-col text-sm p-4 gap-2 rounded-md", {
+const noteVariants = cva("flex flex-col gap-2 rounded-md p-4 text-sm", {
   variants: {
     variant: {
-      info: "bg-gray-100/50 border border-gray-400 text-gray-600",
-      success: "bg-green-100/50 border border-green-400 text-green-600",
-      warning: "bg-yellow-100/50 border border-yellow-400 text-yellow-600",
-      error: "bg-red-100/50 border border-red-400 text-red-600",
+      info: "border border-gray-400 bg-gray-100/50 text-gray-600",
+      success: "border border-green-400 bg-green-100/50 text-green-600",
+      warning: "border border-yellow-400 bg-yellow-100/50 text-yellow-600",
+      error: "border border-red-400 bg-red-100/50 text-red-600",
     },
   },
   defaultVariants: { variant: "info" },
