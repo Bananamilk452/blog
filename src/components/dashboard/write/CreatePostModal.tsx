44847fa2d42aee177a8362e58e307499bb4ae35f
Joonsu Yoon <bananamilk452@gmail.com>
2025-10-27 18:44:23 +0900

    feat(editor): 에디터 카테고리 기능 추가

--- src/components/dashboard/write/CreatePostModal.tsx
+++ src/components/dashboard/write/CreatePostModal.tsx
@@ -1,11 +1,15 @@
 "use client";
 
-import { useMutation } from "@tanstack/react-query";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
+import { useEffect, useMemo, useState } from "react";
+import { useForm } from "react-hook-form";
 import { toast } from "sonner";
 
 import { Spinner } from "~/components/Spinner";
 import { Button } from "~/components/ui/button";
+import { CreatableCombobox } from "~/components/ui/combobox";
 import {
   Dialog,
   DialogContent,
@@ -13,7 +17,24 @@ import {
   DialogHeader,
   DialogTitle,
 } from "~/components/ui/dialog";
-import { createPost as createPostAction } from "~/lib/actions/post";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "~/components/ui/form";
+import {
+  createPost as createPostAction,
+  getCategories,
+} from "~/lib/actions/post";
+import {
+  CreatePostForm,
+  CreatePostFormSchema,
+} from "~/types/zod/CreatePostFormSchema";
+
+import type { ComboboxOption } from "~/components/ui/combobox";
 
 interface CreatePostModalProps {
   title: string;
@@ -30,8 +51,42 @@ export function CreatePostModal({
 }: CreatePostModalProps) {
   const router = useRouter();
 
+  const form = useForm<CreatePostForm>({
+    resolver: zodResolver(CreatePostFormSchema),
+    defaultValues: {
+      title,
+      content,
+      category: "",
+    },
+  });
+
+  useEffect(() => {
+    if (title || content) {
+      form.reset({
+        title,
+        content,
+        category: form.getValues("category") || "",
+      });
+    }
+  }, [title, content, form]);
+
+  const { data: categories } = useQuery({
+    queryKey: ["categories"],
+    queryFn: getCategories,
+  });
+  const categoryOptions = useMemo(() => {
+    return categories
+      ? categories.map((c) => ({ label: c.name, value: c.name }))
+      : [];
+  }, [categories]);
+
   const { mutate: createPost, status: createPostStatus } = useMutation({
-    mutationFn: (data: { title: string; content: string; state: string }) => {
+    mutationFn: (data: {
+      title: string;
+      content: string;
+      state: string;
+      category: string;
+    }) => {
       return createPostAction(data);
     },
     onSuccess: (data, variables) => {
@@ -49,42 +104,69 @@ export function CreatePostModal({
     },
   });
 
+  const handleSubmit = (state: "draft" | "published") => {
+    const values = form.getValues();
+    createPost({ ...values, state });
+  };
+
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle>포스트 작성</DialogTitle>
-        </DialogHeader>
-        <div>포스트를 작성하시겠습니까?</div>
-
-        <DialogFooter>
-          {createPostStatus === "pending" && <Spinner className="mr-2" />}
-          <Button
-            type="submit"
-            onClick={() => createPost({ title, content, state: "published" })}
-            disabled={createPostStatus === "pending"}
-          >
-            작성
-          </Button>
-          <Button
-            variant="white"
-            type="submit"
-            onClick={() => createPost({ title, content, state: "draft" })}
-            disabled={createPostStatus === "pending"}
-          >
-            임시 저장
-          </Button>
-          <Button
-            variant="outline"
-            type="button"
-            onClick={() => {
-              setOpen(false);
-            }}
-            disabled={createPostStatus === "pending"}
-          >
-            취소
-          </Button>
-        </DialogFooter>
+        <Form {...form}>
+          <form>
+            <DialogHeader>
+              <DialogTitle>포스트 발행</DialogTitle>
+            </DialogHeader>
+            <div className="mt-2 grid gap-4 py-4">
+              <FormField
+                control={form.control}
+                name="category"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>카테고리</FormLabel>
+                    <FormControl>
+                      <CreatableCombobox
+                        options={categoryOptions}
+                        value={field.value}
+                        onChange={field.onChange}
+                        placeholder="카테고리를 선택 또는 입력하세요..."
+                        searchPlaceholder="카테고리 검색..."
+                        emptyPlaceholder="일치하는 카테고리가 없습니다."
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <DialogFooter>
+              {createPostStatus === "pending" && <Spinner className="mr-2" />}
+              <Button
+                type="button"
+                onClick={form.handleSubmit(() => handleSubmit("published"))}
+                disabled={createPostStatus === "pending"}
+              >
+                발행
+              </Button>
+              <Button
+                variant="white"
+                type="button"
+                onClick={form.handleSubmit(() => handleSubmit("draft"))}
+                disabled={createPostStatus === "pending"}
+              >
+                임시 저장
+              </Button>
+              <Button
+                variant="outline"
+                type="button"
+                onClick={() => setOpen(false)}
+                disabled={createPostStatus === "pending"}
+              >
+                취소
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
       </DialogContent>
     </Dialog>
   );
--- src/lib/actions/post.ts
+++ src/lib/actions/post.ts
@@ -1,5 +1,6 @@
 "use server";
 
+import { getCategories as getCategoriesFromModel } from "../models/post";
 import { PostService } from "../services/post";
 import { getOptionalSession, getValidAdminSession } from "../utils-server";
 
@@ -7,6 +8,7 @@ export async function createPost(data: {
   title: string;
   content: string;
   state: string;
+  category: string;
 }) {
   const session = await getValidAdminSession();
 
@@ -21,4 +23,8 @@ export async function getPost(id: string) {
   const postService = new PostService(session?.user.id);
 
   return await postService.getPost(id);
+}
+
+export async function getCategories() {
+  return await getCategoriesFromModel();
 }
