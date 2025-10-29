173a93c70fcd439e8e3aed54a48f86a9db468b1d
Joonsu Yoon <bananamilk452@gmail.com>
2025-10-29 14:41:24 +0900

    feat(frontend): getPosts 액션 추가

--- src/lib/actions/post.ts
+++ src/lib/actions/post.ts
@@ -24,6 +24,20 @@ export async function getPost(id: string) {
   return await postService.getPost(id);
 }
 
+export async function getPosts(
+  options: Parameters<typeof PostService.prototype.getPosts>[0],
+) {
+  const session = await getOptionalSession();
+
+  if (session?.user.role !== "admin" && options?.include?.draft) {
+    throw new Error("Unauthorized");
+  }
+
+  const postService = new PostService(session?.user.id);
+
+  return await postService.getPosts(options);
+}
+
 export async function getCategories() {
   const postService = new PostService();
 
