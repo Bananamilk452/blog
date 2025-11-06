2b5beac4664deb593a5eccd92d298f4be9a5fd35
Seoa Yoon <bananamilk452@gmail.com>
2025-11-06 11:45:19 +0900

    fix(frontend): PostList에서 잘못된 주소를 가르키던 버그

--- src/components/dashboard/PostPaginationList.tsx
+++ src/components/dashboard/PostPaginationList.tsx
@@ -43,7 +43,7 @@ export function PostPaginationList({ includeDraft }: PostPaginationListProps) {
               <tr key={post.id} className="*:px-2 *:py-3">
                 <td className="md:w-2/3">
                   <div className="flex gap-2">
-                    <Link className="group" href={`/post/${post.id}`}>
+                    <Link className="group" href={`/post/${post.slug}`}>
                       <span className="font-medium group-hover:underline">
                         {post.title}
                       </span>
