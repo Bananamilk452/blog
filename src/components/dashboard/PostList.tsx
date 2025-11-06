29c95e6932b523bdd8543cf4ff571ddf702ca0c3
Seoa Yoon <bananamilk452@gmail.com>
2025-11-06 21:20:12 +0900

    feat(dashboard): 새 글 작성 버튼 추가

--- src/components/dashboard/PostList.tsx
+++ src/components/dashboard/PostList.tsx
@@ -1,4 +1,14 @@
-import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
+import { PlusIcon } from "lucide-react";
+import Link from "next/link";
+
+import { Button } from "../ui/button";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "../ui/card";
 import { PostPaginationList } from "./PostPaginationList";
 
 export function PostList() {
@@ -6,6 +16,13 @@ export function PostList() {
     <Card>
       <CardHeader>
         <CardTitle>글 목록</CardTitle>
+        <CardAction>
+          <Link href="/dashboard/write">
+            <Button>
+              <PlusIcon />새 글 작성
+            </Button>
+          </Link>
+        </CardAction>
       </CardHeader>
       <CardContent>
         <PostPaginationList includeDraft={true} />
